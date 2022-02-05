0ebf3faa408ebc75257f7619c7cd8bb59dd5cc47
Mehmet Ridvan <mehmetridvanyildirim@gmail.com>
2022-02-05 17:33:15 +0300

    Utility code added
    Object pooling and generic singleton scripts added

---
+++ Assets/Scripts/Utilities/ObjectPool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mryildirim.Utilities
+{
+    public class ObjectPool : Singleton<ObjectPool>
+    {
+        private Dictionary<string, Queue<GameObject>> _poolDict = new Dictionary<string, Queue<GameObject>>();
+
+        public GameObject GetObject(GameObject clone)
+        {
+            if (_poolDict.TryGetValue(clone.name, out Queue<GameObject> objectList))
+            {
+                if (objectList.Count == 0)
+                {
+                    return CreateNewObject(clone);
+                }
+                
+                var obj = objectList.Dequeue();
+                obj.SetActive(true);
+                return obj;
+            }
+            return CreateNewObject(clone);
+        }
+
+        public void ReturnGameObject(GameObject clone)
+        {
+            if (_poolDict.TryGetValue(clone.name, out var objectList))
+            {
+                objectList.Enqueue(clone);
+            }
+            else
+            {
+                var objectQueue = new Queue<GameObject>();
+                objectQueue.Enqueue(clone);
+                _poolDict.Add(clone.name, objectQueue);
+            }
+            clone.SetActive(false);
+        }
+
+        private GameObject CreateNewObject(GameObject clone)
+        {
+            var obj = Instantiate(clone);
+            obj.name = clone.name;
+            return obj;
+        }
+    }
+}
