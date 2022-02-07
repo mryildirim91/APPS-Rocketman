9b96a4abde56a886b6df4ad4d7390394a39b4819
Mehmet Ridvan <mehmetridvanyildirim@gmail.com>
2022-02-07 15:58:49 +0300

    Camera controller optimized

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -5,16 +5,26 @@ namespace Mryildirim.Core
 {
     public class CameraController : MonoBehaviour
     {
-        [SerializeField] private CinemachineVirtualCamera _rocketmanCamera;
+        [SerializeField] private CinemachineVirtualCamera _rocketmanCamera, _sideCamera;
 
         private void OnEnable()
         {
-            EventManager.OnRocketmanLaunched += () => _rocketmanCamera.Priority = 3;
+            EventManager.OnRocketmanLaunched += () =>
+            {
+                _rocketmanCamera.Priority = 3;
+                if(_sideCamera !=null)
+                    _sideCamera.gameObject.SetActive(false);
+            };
         }
 
         private void OnDisable()
         {
-            EventManager.OnRocketmanLaunched -= () => _rocketmanCamera.Priority = 3;
+            EventManager.OnRocketmanLaunched -= () =>
+            {
+                _rocketmanCamera.Priority = 3;
+                if(_sideCamera !=null)
+                    _sideCamera.gameObject.SetActive(false);
+            };
         }
     }
 }
